88cbcea2fe6bcb0026e159b3940b75a74986efe3
Aaron-Doran <wesley.gossett@gmail.com>
2020-05-11 20:44:40 -0400

    Set up the foundatiobn of our cloud database using MongoDB Atlas, MongoDB compass, and adding mongoose to the dependencies.

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
-//Route files
-const bootcamps = require('./routes/bootcamps')
-    
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
-
-
 
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
@@ -22,7 +24,14 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 4000
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`)
 );
+
+// Handle unhandled promise rejections 
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //  Close server & exit process
+    server.close(() => process.exit(1));
+})
