b404ab913940dd82b54e2dce59563563fd091acb
Aaron-Doran <wesley.gossett@gmail.com>
2020-08-17 08:06:01 -0400

    Added roles auth.

--- middleware/auth.js
+++ middleware/auth.js
@@ -34,4 +34,14 @@ exports.protect = asyncHandler(async (req, res, next) => {
     } catch (err) {
         return next(new ErrorResponse('Not authorized to access this protected route', 401));
     }
-})
+});
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorResponse(`User role ${req.user.role} is not authorized to acces this route`, 403));
+        }
+        next();
+    }
+}
