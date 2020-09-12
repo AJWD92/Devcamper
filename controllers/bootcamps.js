f03b13a4f597e5c8f071958f2c2ead17da44e57d
Aaron-Doran <wesley.gossett@gmail.com>
2020-09-11 21:03:53 -0400

    auth

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
@@ -29,7 +29,19 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route    POST /api/v1/bootcamps/
 // @access   Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+    // Add user to req.body
+    req.body.user = req.user.id;
+
+    // Check for published bootcamp
+    const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id })
+    
+    // If the user is not  an admin they can only add one bootcamp
+    if (publishedBootcamp && req.user.role === 'admin') {
+        return next(new ErrorResponse(`The user with id: ${req.user.id} has already published a bootcamp`, 400));
+    }
+
     const bootcamp = await Bootcamp.create(req.body);
+
 
     res.status(201).json({
         success: true,
